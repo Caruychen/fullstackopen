be5d78b2066bd2d633c052c153d27e7e957c5c45
Caruychen <caruychen@gmail.com>
2021-02-11 14:28:17 +0200

    refactor

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -4,7 +4,9 @@ import Part from './Part';
 const Content = ({ course }) => {
   return (
     <div>
-      {course.parts.map(part => <Part key={part.id} part={part} />)}
+      {course.parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
     </div>
   )
 }
--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Total = ({ course }) => {
   const exercisesReducer = (sum, currentValue) => sum + currentValue.exercises;
+
   const total = course.parts.reduce(exercisesReducer, 0);
-  return (
-    <p>total of {total} exercises</p>
-  )
+
+  return <p>total of {total} exercises</p>
 }
 
 export default Total;
