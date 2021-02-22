0ba48b0a28892ec0d4d1d166428b2398cb615ee0
Caruychen <caruychen@gmail.com>
2021-02-22 14:38:03 +0200

    add mongoose connection event handlers

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,7 +7,14 @@ const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 const mongoUrl = config.MONGODB_URI
+logger.info('connecting to:', mongoUrl)
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
