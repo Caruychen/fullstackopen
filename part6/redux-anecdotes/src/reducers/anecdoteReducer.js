d080fcf2d6463690687c5cbc10302f6f588817b9
Caruychen <caruychen@gmail.com>
2021-03-10 14:21:54 +0200

    6.4: anecdotes, step2

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,6 +23,9 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
   switch (action.type) {
+    case 'NEW_ANECDOTE': {
+      return [...state, action.data]
+    }
     case 'INCREMENT': {
       const id = action.data.id
       const anecdoteToChange = state.find(anecdote => anecdote.id === id)
@@ -33,6 +36,13 @@ const reducer = (state = initialState, action) => {
       return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
     }
     default: return state
+  }
+}
+
+export const createAnecdote = (anecdote) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: asObject(anecdote)
   }
 }
 
