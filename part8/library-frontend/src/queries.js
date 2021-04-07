ffd421422957308dc2e1da7f36b707b11fc51020
Caruychen <caruychen@gmail.com>
2021-04-07 17:13:22 +0300

    8.10: Adding a book

---
+++ part8/library-frontend/src/queries.js
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+query {
+  allAuthors {
+    name
+    id
+    born
+    bookCount
+  }
+}
+`
+
+export const ALL_BOOKS = gql`
+query {
+  allBooks {
+    title
+    author
+    published
+    id
+  }
+}
+`
+
+export const CREATE_BOOK = gql`
+mutation createBook($title: String!, $author: String!, $publishedInt: Int!, $genres: [String!]!) {
+  addBook(
+    title: $title,
+    author: $author,
+    published: $publishedInt,
+    genres: $genres
+  ) {
+    title
+    author
+    published
+    genres
+  }
+}
+`
