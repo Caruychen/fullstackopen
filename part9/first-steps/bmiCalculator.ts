0be04e59eca03bffbf09da242956403c05cdf0cf
Caruychen <caruychen@gmail.com>
2021-04-16 14:22:50 +0300

    9.3 Command line

--- part9/first-steps/bmiCalculator.ts
+++ part9/first-steps/bmiCalculator.ts
@@ -1,3 +1,8 @@
+interface bmiInputs {
+  height: number
+  weight: number
+}
+
 const calculateBmi = (height: number, weight: number): string => {
   const heightMeters = height / 100
   const bmi = weight / (heightMeters * heightMeters)
@@ -13,4 +18,25 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 }
 
-console.log(calculateBmi(180, 74))
+const parseBmiInputs = (args: Array<string>): bmiInputs => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  }
+  else {
+    throw new Error('Inputs were not numbers')
+  }
+}
+
+try {
+  const { height, weight } = parseBmiInputs(process.argv)
+  console.log(calculateBmi(height, weight))
+}
+catch (error) {
+  console.log('Oops, something went wrong:', error.message)
+}
--- part9/first-steps/exerciseCalculator.ts
+++ part9/first-steps/exerciseCalculator.ts
@@ -13,6 +13,11 @@ interface Rating {
   ratingDescription: string,
 }
 
+interface exerciseInputs {
+  exerciseHours: Array<number>,
+  target: number
+}
+
 const rater = (average: number, target: number): Rating => {
   switch (true) {
     case average < target - 1:
@@ -41,4 +46,24 @@ const calculateExercises = (exerciseHours: Array<number>, target: number): Resul
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+const parseExerciseInputs = (args: Array<string>): exerciseInputs => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  const exerciseHours = args.slice(3).map(arg => Number(arg))
+  const target = Number(args[2])
+  if (!isNaN(target) && exerciseHours.every(e => !isNaN(e))) {
+    return {
+      exerciseHours,
+      target
+    }
+  } else {
+    throw new Error('Inputs were not numbers')
+  }
+}
+
+
+try {
+  const { exerciseHours, target } = parseExerciseInputs(process.argv)
+  console.log(calculateExercises(exerciseHours, target))
+} catch (error) {
+  console.log('Oops, something went wrong:', error.message)
+}
