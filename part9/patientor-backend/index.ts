34dd776e5fcaf5d7de6f27688f033656b3d06629
Caruychen <caruychen@gmail.com>
2021-04-19 10:21:40 +0300

    9.9: Patientor backend, step2

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
+});
+
+app.get('/api/patients', (_req, _res) => {
+  console.log('getting patient records');
 });
 
 app.listen(PORT, () => {
