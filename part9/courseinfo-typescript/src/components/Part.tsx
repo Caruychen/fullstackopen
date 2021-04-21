220053a76ecd79a4cc2758fac543547e8011b57c
Caruychen <caruychen@gmail.com>
2021-04-21 14:20:15 +0300

    9.15. remove unnecessary divs

--- part9/courseinfo-typescript/src/components/Part.tsx
+++ part9/courseinfo-typescript/src/components/Part.tsx
@@ -11,47 +11,39 @@ const Part = ({ part }: { part: CoursePart }) => {
   switch (part.type) {
     case "normal":
       return (
-        <div>
-          <p>
-            <b>{part.name} {part.exerciseCount}</b>
-            <br />
-            <em>{part.description}</em>
-          </p>
-        </div>
+        <p>
+          <b>{part.name} {part.exerciseCount}</b>
+          <br />
+          <em>{part.description}</em>
+        </p>
       )
     case "groupProject":
       return (
-        <div>
-          <p>
-            <b>{part.name} {part.exerciseCount}</b>
-            <br />
-            <em>project exercises {part.groupProjectCount}</em>
-          </p>
-        </div>
+        <p>
+          <b>{part.name} {part.exerciseCount}</b>
+          <br />
+          <em>project exercises {part.groupProjectCount}</em>
+        </p>
       )
     case "submission":
       return (
-        <div>
-          <p>
-            <b>{part.name} {part.exerciseCount}</b>
-            <br />
-            <em>{part.description}</em>
-            <br />
+        <p>
+          <b>{part.name} {part.exerciseCount}</b>
+          <br />
+          <em>{part.description}</em>
+          <br />
             submit to {part.exerciseSubmissionLink}
-          </p>
-        </div>
+        </p>
       )
     case "special":
       return (
-        <div>
-          <p>
-            <b>{part.name} {part.exerciseCount}</b>
-            <br />
-            <em>{part.description}</em>
-            <br />
+        <p>
+          <b>{part.name} {part.exerciseCount}</b>
+          <br />
+          <em>{part.description}</em>
+          <br />
             required skills: {part.requirements.join(', ')}
-          </p>
-        </div>
+        </p>
       )
     default:
       return assertNever(part);
